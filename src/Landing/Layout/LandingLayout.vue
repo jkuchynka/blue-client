d7816a9e8f199e355f43887c224fbec14d4df71d
Jason Kuchynka <jason.kuchynka@gmail.com>
2020-04-22 21:03:07 -0700

    Setup initial Landing module

---
+++ src/Landing/Layout/LandingLayout.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+  .landing-layout
+    q-layout(view="lHh Lpr lff")
+      q-header
+        q-toolbar
+
+          q-toolbar-title
+            router-link(to="/") Blue
+
+          q-btn(flat to="/pricing") Pricing
+
+          q-btn(flat to="/features") Features
+
+          q-space
+
+          q-btn(flat to="/login") Login
+
+          q-btn(flat to="/register") Register
+
+      .hero-background-wrapper
+        .hero-background
+
+        .content-wrapper
+          router-view
+</template>
+<script>
+export default {
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="sass" scoped>
+
+.landing-layout
+  background: blue
+
+.q-header
+  position: absolute
+  top: 0
+  left: 0
+  right: 0
+  z-index: 2
+  background: none
+  .q-toolbar__title a
+    color: #fff
+    text-decoration: none
+
+.hero-background-wrapper
+  min-height: 100vh
+  display: flex
+  align-items: center
+  flex-direction: column
+  // background-image linear-gradient(-280deg, #3f51b5, #a832d7)
+  // background-image linear-gradient(-280deg, $cyan-6 , $deep-purple-4)
+  background-image linear-gradient(-280deg, $cyan-2, $deep-purple-6)
+  // background-image: linear-gradient(-280deg, $cyan-2, $blue-10)
+
+.hero-background
+  background-repeat: no-repeat
+  background-position: 0% 28%
+  background-size: cover
+  position: absolute
+  height: 100%
+  width: 100%
+  background-image: url(~@/Landing/assets/hero-bg.jpg)
+  mix-blend-mode: overlay
+  opacity: 0.5
+
+.content-wrapper
+  width: 880px
+  z-index: 9
+  padding-top: 200px
+  text-align: center
+  position: relative
+  color: #fff
+
+</style>
---
+++ src/Landing/Page/LandingPage.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+  .landing-page
+
+    .intro
+      .intro-message Welcome to Blue.
+      .intro-message-sub A full-fledged spa starter app built with Quasar (VueJS) and Laravel API.
+
+      .intro-message-about
+        .bg
+        .text The frontend and the backend are separate projects, which allows for using them separately. Together, they provide a fully working landing page, auth flow and admin.
+</template>
+<script>
+export default {
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="sass" scoped>
+
+.intro-message
+  font-family: 'Jomolhari', serif
+  font-size: 48px
+  line-height: 48px
+
+.intro-message-sub
+  font-size: 22px
+  margin: 12px 0
+
+.intro-message-about
+  font-size: 18px
+  margin: 24px 0
+  padding: 24px
+  text-align: left
+  position: relative
+  color: $grey-10
+  .text
+    position: relative
+    z-index: 9
+  .bg
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+    background: $grey-1
+    opacity: 0.8
+    z-index: 1
+</style>
